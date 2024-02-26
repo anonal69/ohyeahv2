0b431739065ee62154fa38c698466c08772486e3
kuyol-project <153427860+BUILDPROJECT222@users.noreply.github.com>
2024-02-26 16:07:47 +0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -13,7 +13,7 @@ export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
 export const PLATFORM_SHARABLE_URL = "play-Solcade";
 
 // Toggle all live events acrossed gamba toast
-export const LIVE_EVENT_TOAST = false;
+export const LIVE_EVENT_TOAST = true;
 
 // RPC - Can be configured in .env
 export const RPC_ENDPOINT =
