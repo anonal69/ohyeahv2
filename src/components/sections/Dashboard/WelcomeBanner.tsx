55073d6e54729910beb0b55168e5144bbb823a36
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-04-02 21:35:59 -0400

    Update WelcomeBanner.tsx

--- src/components/sections/Dashboard/WelcomeBanner.tsx
+++ src/components/sections/Dashboard/WelcomeBanner.tsx
@@ -30,7 +30,20 @@ export function WelcomeBanner() {
             Twitter
           </button>
         </Buttons>
-
+          <button className="creative-button" onClick={() => window.open('https://t.me/KOOL_AID_MAN_SOL', '_blank')}>
+            Telegram
+          </button>
+          <button className="creative-button" onClick={() => window.open('https://dexscreener.com/solana/3zbz2skx3byyfgfxkvvqmcnumbqv2gtqau79vfbzyqv9', '_blank')}>
+            Dexscreener
+          </button>
+        </Buttons>
+          <button className="creative-button" onClick={() => window.open('https://kool-aid.xyz/', '_blank')}>
+            Website
+          </button>
+          <button className="creative-button" onClick={() => window.open('https://magiceden.io/marketplace/juic3dnfts', '_blank')}>
+            JUIC3D NFTs
+          </button>
+        </Buttons>
       </div>
     </Welcome>
   );
